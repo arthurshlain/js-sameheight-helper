3c40f35268c756a4a7ac06423aa8363f383bd8a8
Arthur Shlain <artur.slain@gmail.com>
2019-02-27 16:12:17 +0500

    Create sameheight.js

---
+++ sameheight.js
@@ -0,0 +1,33 @@
+function sameHeight() {
+    $('.same-height-group-heading').each(function () {
+        var sameHeightGroup = $(this);
+        var sameHeightDivs = sameHeightGroup.find('.same-height-heading');
+        var maxHeight = 0;
+        sameHeightDivs.css('height', 'auto');
+        sameHeightDivs.each(function () {
+            if ($(this).height() > maxHeight) {
+                maxHeight = $(this).height();
+            }
+        });
+        sameHeightDivs.height(maxHeight);
+    });
+    $('.same-height-group').each(function () {
+        var sameHeightGroup = $(this);
+        var sameHeightDivs = sameHeightGroup.find('.same-height');
+        var maxHeight = 0;
+        sameHeightDivs.css('height', 'auto');
+        sameHeightDivs.each(function () {
+            if ($(this).height() > maxHeight) {
+                maxHeight = $(this).height();
+            }
+        });
+        sameHeightDivs.height(maxHeight);
+    });
+}
+
+jQuery(function ($) {
+  sameHeight();
+  window.addEventListener('resize', function(){
+      sameHeight();
+  }, true);
+});
