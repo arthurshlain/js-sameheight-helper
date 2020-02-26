7369b4e7aac8ecbbea4082a7cc826788e41da8bb
Arthur Shlain <artur.slain@gmail.com>
2020-02-26 23:28:21 +0500

    Update sameheight.js

--- sameheight.js
+++ sameheight.js
@@ -1,22 +1,6 @@
 function sameHeight() {
-  var i, j, headings, blocks, items;
-  
-  headings = document.getElementsByClassName('same-height-group-heading');
+  var i, j, blocks, items;
   blocks = document.getElementsByClassName('same-height-group');
-  
-  for (i = 0; i < headings.length; i++) {
-    items = headings[i].getElementsByClassName("same-height-heading");
-    var maxHeight = 0;
-    for (j = 0; j < items.length; j++) {
-        items[j].style.height = 'auto';
-        if(items[j].clientHeight > maxHeight){
-          maxHeight = items[j].clientHeight + 'px';
-        }
-    }
-    for (j = 0; j < items.length; j++) {
-      items[j].style.height = maxHeight;
-    }
-  }
   for (i = 0; i < blocks.length; i++) {
     items = blocks[i].getElementsByClassName("same-height");
     var maxHeight = 0;
